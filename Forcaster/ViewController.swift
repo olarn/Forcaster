08c6ce778c91c0f38453104b52602e718bc3a627
Olarn U <olarn.u@gmail.com>
2017-05-18 09:58:09 +0700

    Initial Commit

---
+++ Forcaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Forcaster
+//
+//  Created by Olarn U. on 5/18/2560 BE.
+//  Copyright © 2560 Olarn U. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
